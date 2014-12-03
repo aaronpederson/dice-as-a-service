1c861586558b7fa4a37b22e2bbc6fdfe671b1c79
Chris Olstrom <chris@olstrom.com>
2014-12-03 15:58:33 -0800

    Added cleaner CLI with commander. Also docs.

--- roll.rb
+++ roll.rb
@@ -1,14 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'highline/import'
-
-dice = ARGV[0].to_i
-sides = ARGV[1].to_i
-
-# dice = ask 'How many dice should we roll? ', Integer
-# sides = ask 'How many sides per die? ', Integer
-
-puts "You asked for #{ dice } dice with #{ sides } sides."
+require 'commander'
 
 def roll(dice, sides)
   rolls = []
@@ -22,6 +15,23 @@ def roll(dice, sides)
   return rolls
 end
 
-rolls = roll dice, sides
+Commander.configure do
+  program :name, 'Roll'
+  program :version, '0.1.0'
+  program :description, 'It rolls dice.'
 
-puts "Total rolled was #{ rolls.reduce :+ }"
+  default_command :roll
+
+  command :roll do |command|
+    command.option '--dice INTEGER', Integer, 'Number of dice to roll'
+    command.option '--sides INTEGER', Integer, 'Number of sides per die'
+    command.action do |_args, options|
+      dice = options.dice || ask('How many dice should we roll? ', Integer)
+      sides = options.sides || ask('How many sides per die? ', Integer)
+
+      puts "You asked for #{ dice } dice with #{ sides } sides."
+      rolls = roll dice, sides
+      puts "Total rolled was #{ rolls.reduce :+ }"
+    end
+  end
+end
