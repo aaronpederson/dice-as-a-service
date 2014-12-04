2064a849a7ec8747e84b3f72f0283bbf1c894cb0
Chris Olstrom <chris@olstrom.com>
2014-12-03 17:26:28 -0800

    Moved selection logic from server into client, added primitive (de)serialization.

--- roll-client.rb
+++ roll-client.rb
@@ -8,7 +8,26 @@ context = ZMQ::Context.new
 client = context.socket ZMQ::REQ
 client.connect 'tcp://localhost:5555'
 
-client.send_string '2d20'
-response = ''
-client.recv_string response
-puts "Got #{ response } from server."
+Commander.configure do
+  program :name, 'Roll'
+  program :version, '0.1.0'
+  program :description, 'It rolls dice.'
+
+  default_command :roll
+
+  command :roll do |command|
+    command.option '--dice INTEGER', Integer, 'Number of dice to roll'
+    command.option '--sides INTEGER', Integer, 'Number of sides per die'
+    command.action do |_args, options|
+      dice = options.dice || ask('How many dice should we roll? ', Integer)
+      sides = options.sides || ask('How many sides per die? ', Integer)
+
+      puts "You asked for #{ dice } dice with #{ sides } sides."
+
+      client.send_string "#{ dice }d#{ sides }"
+      response = ''
+      client.recv_string response
+      puts "Got #{ response } from server."
+    end
+  end
+end
--- roll-server.rb
+++ roll-server.rb
@@ -16,34 +16,18 @@ def roll(dice, sides)
   rolls.reduce(:+).to_s
 end
 
-def listen(dice, sides)
+def listen
   context = ZMQ::Context.new
   server = context.socket ZMQ::REP
   server.bind 'tcp://*:5555'
   
   puts 'Dice Server Awaiting Orders!'
   loop do
-    server.recv_string ''
-    server.send_string roll(dice, sides)
+    request = ''
+    server.recv_string request
+    dice, sides = request.split(/d/)
+    server.send_string roll(dice.to_i, sides.to_i)
   end
 end
 
-Commander.configure do
-  program :name, 'Roll'
-  program :version, '0.1.0'
-  program :description, 'It rolls dice.'
-
-  default_command :roll
-
-  command :roll do |command|
-    command.option '--dice INTEGER', Integer, 'Number of dice to roll'
-    command.option '--sides INTEGER', Integer, 'Number of sides per die'
-    command.action do |_args, options|
-      dice = options.dice || ask('How many dice should we roll? ', Integer)
-      sides = options.sides || ask('How many sides per die? ', Integer)
-
-      puts "You asked for #{ dice } dice with #{ sides } sides."
-      listen dice, sides
-    end
-  end
-end
+listen
